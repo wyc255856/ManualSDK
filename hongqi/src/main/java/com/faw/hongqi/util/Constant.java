ffd99bf659863b9b229334b37edc749023c16466
吉润达张晴 <zhang3646532@163.com>
2022-02-17 16:10:41 +0800

    no message

--- hongqi/src/main/java/com/faw/hongqi/util/Constant.java
+++ hongqi/src/main/java/com/faw/hongqi/util/Constant.java
@@ -24,7 +24,8 @@ public class Constant {
     public static String GAME_WEB_URL = "";//互动游戏webview地址
     public static String ZIP_VERSION = "";//36张图资源包版本
     public static String ZIP_URL = "";//36张图资源包下载地址
-    public static String CAR_NAME = "";//车型名称
+    public static String CAR_NAME = "";//车型名称、
+    
     public final static boolean IS_PHONE = true;//判断包是否是手机应用
     public final static boolean DEBUG = true;//是否是调试包
     public final static boolean TEST = true;//是否是不带资源的测试包
