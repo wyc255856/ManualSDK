015297c35819f875ce8a90e294b0912ea4b6e9f8
吉润达张晴 <zhang3646532@163.com>
2022-02-21 19:16:13 +0800

    no message

---
+++ app/src/main/java/com/haowei/wyc/c229sdk/C229API.java
@@ -0,0 +1,46 @@
+package com.haowei.wyc.c229sdk;
+
+import android.content.Context;
+import android.content.Intent;
+
+import com.faw.hongqi.ui.C229WelcomeActivity;
+import com.faw.hongqi.ui.WelcomeActivity;
+import com.faw.hongqi.util.Constant;
+import com.faw.hongqi.util.SharedpreferencesUtil;
+//import com.liulishuo.filedownloader.FileDownloader;
+//import com.raizlabs.android.dbflow.config.FlowManager;
+//import com.raizlabs.android.dbflow.config.hongqiGeneratedDatabaseHolder;
+
+public class C229API {
+    public static void init(Context context) {
+//        FlowManager.init(context);
+//        FileDownloader.setup(context);
+//
+//        FlowManager.initModule(hongqiGeneratedDatabaseHolder.class);
+//        Constant.initData();
+    }
+
+    public static void openManual(Context context) {
+        Intent intent = new Intent(context, C229WelcomeActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK );
+        context.startActivity(intent);
+
+    }
+    public static void openManualVertical(Context context) {
+        Intent intent = new Intent(context, WelcomeActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK );
+        context.startActivity(intent);
+
+    }
+    public static void openManual(Context context, String carType, String trim) {
+        Constant.CAR_TYPE = carType;
+        Constant.INTPROPERTY_TYPE = trim;
+        context.startActivity(new Intent(context, C229WelcomeActivity.class));
+    }
+
+    public static void openManual(Context context, String model) {
+        SharedpreferencesUtil.setCarModel(context, model);
+        openManual(context);
+
+    }
+}
